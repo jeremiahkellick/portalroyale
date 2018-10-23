8abace964d8e1e10349d4bb16e2de1af448932d7
Danny Xu <xu.danny26@gmail.com>
2018-10-23 16:04:06 -0700

    Fixed player collisions

--- frontend/src/game/create_bullet.js
+++ frontend/src/game/create_bullet.js
@@ -7,9 +7,9 @@ import Circle from './circle';
 
 const createBullet = ({id, owned}) => {
   const bullet = new GameObject(id, 2);
-  const transform = new Transform( new Vector(50, 50 ) );
+  const transform = new Transform( new Vector(50, 50));
   bullet.addComponent(transform);
-  bullet.addComponent(new Collider(new Circle(1)));
+  bullet.addComponent(new Collider(new Circle(5)));
   bullet.addComponent(new BulletRenderer());
   return bullet;
 };
