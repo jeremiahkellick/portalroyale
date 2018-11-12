2b68fd8c2d15dea248541c0fcd5b3a9dbbe0d560
Danny Xu <dx@Dannys-MacBook-Pro.local>
2018-11-12 13:25:39 -0800

    Add in the model

---
+++ frontend/src/components/leaderboard/table.jsx
@@ -0,0 +1,20 @@
+class Team extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div>
+                <h1>LEADERBOARDS</h1>
+                <tr>
+                    <td>RANK</td>
+                    <td>PLAYER</td>
+                    <td>MOST KILL</td>
+                </tr>
+            </div>
+        );
+    }
+}
+
+export default Team;
---
+++ frontend/src/components/leaderboard/table_container.jsx
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux';
+import Table from './table';
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect( mapStateToProps, mapDispatchToProps )( Table );
