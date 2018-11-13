b811f226358ae22b6b5e0ae936d33f228149dedb
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-11-13 11:13:43 -0800

    Add demo button that runs the game client-side only

--- frontend/src/actions/game_actions.js
+++ frontend/src/actions/game_actions.js
@@ -18,8 +18,8 @@ export const gameOver = () => ({ type: GAME_OVER });
 
 export const win = () => ({ type: WIN });
 
-export const initializeGame = name => dispatch => {
-  GameUtil.initializeGame(name, dispatch);
+export const initializeGame = (name, online) => dispatch => {
+  GameUtil.initializeGame(name, online, dispatch);
   dispatch(enterLobby(name));
 }
 
