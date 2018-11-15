289f4501566dcb19ed7271f4cbd4cd093480b028
Jingna Li <jingna13@gmail.com>
2018-11-14 23:07:59 -0800

    Add appropriate routes for Navbar

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavBar from './navbar';
+import { Switch, Route } from 'react-router-dom';
 
 const Header = () => (
   <div className="header">
     <header className="App-header">
       <h1>Portal Royale</h1>
     </header>
-    <NavBar />
+    <Switch>
+      <Route path="/lobby" component={ null } />
+      <Route path="/" component={ NavBar } />
+    </Switch>
   </div>
 );
 
