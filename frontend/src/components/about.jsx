a8fafccb96637c04b7bcceeedd1895af8ff79f0c
Jingna Li <jingna13@gmail.com>
2018-11-29 14:48:16 -0800

    Add video to about page

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 const About = () => (
   <div className="about">
+    <div className="video">
+     <video className="tutorial-video" src="./videos/battle_royale_demo.mp4"  autoPlay loop />
+    </div>
+    <br />
     <p>This game is a bird's eye view battle royale game.</p>
   </div>
 );
