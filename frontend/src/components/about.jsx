f5cc963cc8d1403d77c4514ac8e27b6038e1c5aa
Jingna Li <jingna13@gmail.com>
2018-11-29 15:34:08 -0800

    Add placeholder text

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -6,7 +6,6 @@ const About = () => (
      <video className="tutorial-video" src="./videos/battle_royale_demo.mp4"  autoPlay loop />
     </div>
     <br />
-    <p>This game is a bird's eye view battle royale game.</p>
   </div>
 );
 
