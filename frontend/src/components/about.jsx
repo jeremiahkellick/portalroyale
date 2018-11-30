dcc595e192dd7be35fdb233d0d41909be9164d59
Jingna Li <jingna13@gmail.com>
2018-11-29 19:35:43 -0800

    Add video fix and full screen

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div className="about">
     <div className="video">
-     <video className="tutorial-video" src="./videos/battle_royale_demo.mp4"  autoPlay loop />
+     <video className="tutorial-video" src="./videos/battle_royale_demo.mp4"  autoPlay loop controls />
     </div>
     <br />
   </div>
